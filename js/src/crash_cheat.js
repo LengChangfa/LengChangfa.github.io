670f67cad7c8754f11aac0a21d188ed2298fb186
lengchangfa <858180665@qq.com>
2020-07-25 21:40:30 +0800

    Site updated: 2020-07-25 21:40:17

--- js/src/crash_cheat.js
+++ js/src/crash_cheat.js
@@ -1,3 +1,3 @@
-// build time:Fri Jul 24 2020 16:16:16 GMT+0800 (GMT+08:00)
+// build time:Sat Jul 25 2020 21:38:36 GMT+0800 (GMT+08:00)
 window.onload=function(){var e=document.title;var t;document.addEventListener("visibilitychange",function(){if(document.hidden){$('[rel="icon"]').attr("href","../../images/failure.png");$('[rel="shortcut icon"]').attr("href","../../images/failure.png");document.title="(●—●)喔哟，崩溃啦！";clearTimeout(t)}else{$('[rel="icon"]').attr("href","../../images/pig-32×32.gif");$('[rel="shortcut icon"]').attr("href","../../images/pig-32×32.gif");document.title="(/≧▽≦/)咦！页面又好了！";t=setTimeout(function(){document.title=e},2e3)}})};
 //rebuild by neat 
