f988c80a911c614cebc68263dfd6217117771446
lengchangfa <858180665@qq.com>
2020-07-28 02:01:12 +0800

    Site updated: 2020-07-28 02:00:58

--- js/src/clipboard-use.js
+++ js/src/clipboard-use.js
@@ -1,3 +1,3 @@
-// build time:Sat Jul 25 2020 21:53:00 GMT+0800 (GMT+08:00)
+// build time:Tue Jul 28 2020 01:57:16 GMT+0800 (GMT+08:00)
 !function(n,t,a){var i=function(){var n="";n+='<button class="btn-copy" data-clipboard-snippet="">';n+='  <i class="fa fa-clipboard"></i><span>复制</span>';n+="</button>";$(".highlight .code pre").before(n);new ClipboardJS(".btn-copy",{target:function(n){return n.nextElementSibling}})};i()}(window,document);
 //rebuild by neat 
