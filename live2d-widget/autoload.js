672bb27eb38552d886524ab37b3b2c393b88b35a
lengchangfa <858180665@qq.com>
2020-08-20 21:05:52 +0800

    Site updated: 2020-08-20 21:05:39

--- live2d-widget/autoload.js
+++ live2d-widget/autoload.js
@@ -1,3 +1,3 @@
-// build time:Wed Aug 19 2020 11:07:39 GMT+0800 (GMT+08:00)
+// build time:Thu Aug 20 2020 21:05:06 GMT+0800 (GMT+08:00)
 const live2d_path="https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget/";$("<link>").attr({href:live2d_path+"waifu.css",rel:"stylesheet"}).appendTo("head");$.ajax({url:live2d_path+"live2d.min.js",dataType:"script",cache:true});$.ajax({url:live2d_path+"waifu-tips.js",dataType:"script",cache:true});$(window).on("load",function(){initWidget(live2d_path+"waifu-tips.json","https://live2d.fghrsh.net/api")});
 //rebuild by neat 
