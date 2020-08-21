2120c2c231711c0939604a3c6d286e47b02666ea
lengchangfa <858180665@qq.com>
2020-08-21 10:39:01 +0800

    Site updated: 2020-08-21 10:38:34

--- js/src/clipboard-use.js
+++ js/src/clipboard-use.js
@@ -1,3 +1,3 @@
-// build time:Thu Aug 20 2020 21:05:06 GMT+0800 (GMT+08:00)
+// build time:Fri Aug 21 2020 10:37:58 GMT+0800 (GMT+08:00)
 !function(n,t,a){var i=function(){var n="";n+='<button class="btn-copy" data-clipboard-snippet="">';n+='  <i class="fa fa-clipboard"></i><span>复制</span>';n+="</button>";$(".highlight .code pre").before(n);new ClipboardJS(".btn-copy",{target:function(n){return n.nextElementSibling}})};i()}(window,document);
 //rebuild by neat 
